25d358491959852c7afc61c5ceed9cd8fc4d203d
jgclark <jonathan@clarksonline.me.uk>
2021-01-29 11:43:10 +0000

    Add tidyClippings script

---
+++ tidyClippings.rb
@@ -0,0 +1,155 @@
+#!/usr/bin/ruby
+#-------------------------------------------------------------------------------
+# Script to tidy and clean up HTML text clipped into markdown files 
+# by Jonathan Clark, v1.0.0, 29.1.2021
+#-------------------------------------------------------------------------------
+VERSION = "1.0.0"
+require 'date'
+require 'cgi'
+require 'colorize'
+require 'optparse' # more details at https://docs.ruby-lang.org/en/2.1.0/OptionParser.html
+
+#-------------------------------------------------------------------------------
+# Setting variables to tweak
+#-------------------------------------------------------------------------------
+NOTE_EXT = "md" # or "txt"
+IFTTT_FILEPATH = "/Users/jonathan/Dropbox/IFTTT/Clips"
+IFTTT_ARCHIVE_FILEPATH = "/Users/jonathan/Dropbox/IFTTT/Archive"
+DATE_TIME_LOG_FORMAT = '%e %b %Y %H:%M'.freeze # only used in logging
+
+#-------------------------------------------------------------------------------
+# To use test data instead of live data, uncomment relevant definitions:
+#-------------------------------------------------------------------------------
+# twitter_test_data = <<-END_T_DATA
+# January 24, 2021 at 03:56PM | A useful thread which highlights the problems of asking the wrong question. https://t.co/xEKimf9cLK | jgctweets | http://twitter.com/jgctweets/status/1353371090609909760
+# END_T_DATA
+
+#-------------------------------------------------------------------------------
+# Other Constants & Settings
+#-------------------------------------------------------------------------------
+USERNAME = ENV['LOGNAME'] # pull username from environment
+USER_DIR = ENV['HOME'] # pull home directory from environment
+DROPBOX_DIR = "#{USER_DIR}/Dropbox/Apps/NotePlan/Documents".freeze
+ICLOUDDRIVE_DIR = "#{USER_DIR}/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents".freeze
+CLOUDKIT_DIR = "#{USER_DIR}/Library/Containers/co.noteplan.NotePlan3/Data/Library/Application Support/co.noteplan.NotePlan3".freeze
+# TodaysDate = Date.today # can't work out why this needs to be a 'constant' to work -- something about visibility, I suppose
+np_base_dir = CLOUDKIT_DIR if Dir.exist?(CLOUDKIT_DIR) && Dir[File.join(CLOUDKIT_DIR, '**', '*')].count { |file| File.file?(file) } > 1
+NP_CALENDAR_DIR = "#{np_base_dir}/Calendar".freeze
+
+# Colours to use with the colorization gem
+# to show some possible combinations, run  String.color_samples
+# to show list of possible modes, run   puts String.modes  (e.g. underline, bold, blink)
+String.disable_colorization false
+CompletedColour = :light_green
+InfoColour = :yellow
+WarningColour = :light_red
+
+# Variables that need to be globally available
+time_now = Time.now
+$date_time_now_log_fmttd = time_now.strftime(DATE_TIME_LOG_FORMAT)
+$verbose = false
+$npfile_count = 0
+
+#=======================================================================================
+# Main logic
+#=======================================================================================
+
+# Setup program options
+options = {}
+opt_parser = OptionParser.new do |opts|
+  opts.banner = "Tidy web clippings v#{VERSION}"
+  opts.separator ''
+  # options[:instapaper] = false
+  opts.on('-h', '--help', 'Show this help') do
+    puts opts
+    exit
+  end
+  opts.on('-v', '--verbose', 'Show information as I work [on by default at the moment]') do
+    options[:verbose] = true
+    $verbose = true
+  end
+end
+opt_parser.parse! # parse out options, leaving file patterns to process
+
+puts "Starting to tidy web clippings at #{$date_time_now_log_fmttd}."
+
+#-------------------------------------------------------------------------
+# Read .txt files in the directory
+#-------------------------------------------------------------------------
+begin
+  Dir.chdir(IFTTT_FILEPATH)
+  Dir.glob("*.txt").each do |this_file|
+    puts "- #{this_file}".colorize(InfoColour) if $verbose
+
+    # initialise other variables (that don't need to persist with the class)
+    n = 0
+    lines = []
+    # Open file and read in all lines (finding any Done and Cancelled headers)
+    # NB: needs the encoding line when run from launchctl, otherwise you get US-ASCII invalid byte errors (basically the 'locale' settings are different)
+    f = File.open(this_file, 'r', encoding: 'utf-8')
+    f.each_line do |line|
+    lines[n] = line
+      # puts " #{n}: #{line}" if $verbose
+      line_in = line.clone  # need a proper clone, not just a reference
+
+      # Delete #clipped or #popclipped references at start of a line
+      lines.delete_at(n) if line =~ /^#popclipped$/i || line =~ /^#clipped$/i
+
+      # replace HTML entity elements with ASCII equivalents
+      line.gsub!(/&amp;/, '&')
+      line.gsub!(/&nbsp;/, ' ')
+      line.gsub!(/&nbsp_place_holder;/, ' ')
+      line.gsub!(/&mdash;/, '--')
+      line.gsub!(/&lsquot;/, "\'")
+      line.gsub!(/&ldquot;/, "\"")
+      line.gsub!(/&rsquot;/, "\'")
+      line.gsub!(/&rdquot;/, "\"")
+      line.gsub!(/&quot;/, "\"")
+      line.gsub!(/&lt;/, "<")
+      line.gsub!(/&gt;/, ">")
+      line.gsub!(/&hellip;/, "...")
+      line.gsub!(/&nbsp;/, " ")
+
+      # replace smart quotes with dumb ones
+      line.gsub!(/“/, '"')
+      line.gsub!(/”/, '"')
+      line.gsub!(/‘/, '\'')
+      line.gsub!(/’/, '\'')
+      # replace en dash with markdwon equivalent
+      line.gsub!(/—/, '--')
+      # replace '***' or '* * *' with '---'
+      line.gsub!(/\* \* \*/, '---')
+      line.gsub!(/\*\*\*/, '---')
+      # replace a line just surrounded by **...** with an H4 instead
+      line.gsub!(/^\s*\*\*(.*)\*\*\s*$/, '#### \1')
+      # replace asterisk lists with dash lists (to stop NP thinking they are tasks)
+      line.gsub!(/^(\s*)\*\s/, '\1-')
+
+      puts "  #{n}: #{line_in.chomp}\n   -> #{line}" if $verbose && line_in != line
+
+      n += 1
+    end
+    f.close
+
+    line_count = lines.size # e.g. for lines 0-2 size => 3
+    puts "  Read file '#{this_file}'"
+
+    #-------------------------------------------------------------------------
+    # write out this updated file, as a markdown file
+    #-------------------------------------------------------------------------
+    # open file and write all the lines out
+    new_filename = "#{this_file[0..-5]}.#{NOTE_EXT}" # take off .txt and put on .md
+    File.open(new_filename, 'w') do |f|
+      lines.each do |line|
+        f.puts line
+      end
+    end
+    puts '  -> written updated version to ' + this_file.to_s.colorize(CompletedColour)
+
+    # Now rename file to same as above but _YYYYMMDDHHMM on the end
+    archive_filename = "#{IFTTT_ARCHIVE_FILEPATH}/#{this_file}"
+    File.rename(this_file, archive_filename)
+  end
+rescue StandardError => e
+  puts "ERROR: #{e.exception.message}".colorize(WarningColour)
+end
